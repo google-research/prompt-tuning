536a71925e78153b3978e1771bda128df6d0ba5a
Brian Lester <brianlester@google.com>
2022-02-02 14:43:09 -0800

    Update numpy checkpoint saving to handle when there is already a directory with the same name.
    It is possible for there to be a crash or preemption between the numpy checkpoint we save the learned prompt with and the t5x checkpoint that is used for preemption recovery. This means when you restart training, the last good t5x checkpoint will be loaded, training will happen, and then we will try to save a new numpy checkpoint with the original step value where the crash happened. This change updates the numpy saving to log a warning if that happens, rename the old checkpoint to a backup directory, and then save the new current checkpoint to the step value. There is also a test for this behavior.
    
    PiperOrigin-RevId: 425982265

--- prompt_tuning/train/utils_test.py
+++ prompt_tuning/train/utils_test.py
@@ -14,6 +14,7 @@
 
 """Tests for utils."""
 
+import inspect
 import operator
 import os
 from unittest import mock
@@ -37,7 +38,6 @@ def create_fake_parameters():
   parameters['encoder/prompt/prompt'] = np.random.rand(p_length, embed_dim)
   parameters = flax.core.freeze(traverse_util.unflatten_dict(
       {tuple(k.split('/')): v for k, v in parameters.items()}))
-  print(parameters)
   return parameters
 
 
@@ -105,6 +105,98 @@ class UtilsTest(absltest.TestCase):
         time_mock.return_value = time
         checkpointer.save_numpy(parameters, step=step)
 
+    self.assertLen(save_mock.call_args_list, len(golds))
+    call_args = sorted((call_args[0] for call_args in save_mock.call_args_list),
+                       key=operator.itemgetter(0))
+    for (saved_path, saved_var), gold in zip(call_args, golds):
+      self.assertEqual(saved_path, os.path.join(checkpoint_dir, gold[0]))
+      np.testing.assert_allclose(saved_var,
+                                 np.array(gold[1]).astype(np.float32))
+
+  def test_checkpointer_with_existing_numpy_checkpoint(self):
+    parameters = create_fake_parameters()
+    checkpoint_dir = self.create_tempdir().full_path
+    time = 14
+    backup_time = 16
+    step = 42
+    golds = [
+        (f'{checkpoint_dir}/numpy_checkpoints/checkpoint_{step}.tmp-{time}/'
+         'encoder.layers_0.attention.key.kernel',
+         parameters['encoder']['layers_0']['attention']['key']['kernel']),
+        (f'{checkpoint_dir}/numpy_checkpoints/checkpoint_{step}.tmp-{time}/'
+         'encoder.layers_1.attention.query.kernel',
+         parameters['encoder']['layers_1']['attention']['query']['kernel']),
+        (f'{checkpoint_dir}/numpy_checkpoints/checkpoint_{step}.tmp-{time}/'
+         'encoder.layers_2.attention.value.kernel',
+         parameters['encoder']['layers_2']['attention']['value']['kernel']),
+        (f'{checkpoint_dir}/numpy_checkpoints/checkpoint_{step}.tmp-{time}/'
+         'encoder.layers_3.attention.out.kernel',
+         parameters['encoder']['layers_3']['attention']['out']['kernel']),
+        (f'{checkpoint_dir}/numpy_checkpoints/checkpoint_{step}.tmp-{time}/'
+         'encoder.prompt.prompt', parameters['encoder']['prompt']['prompt'])]
+    variable_paths = ['.*/prompt/.*',
+                      '.*/layers_0/.*/key/.*',
+                      '.*/layers_1/.*/query/.*',
+                      '.*/layers_2/.*/value/.*',
+                      '.*/layers_3/.*/out/.*']
+
+    def mock_init(self, save_paths):
+      self.checkpoints_dir = checkpoint_dir
+      self.save_matcher = utils.match_any(save_paths)
+      self._save_dtype = np.float32
+
+    with mock.patch.object(utils.Checkpointer, '__init__', new=mock_init):
+      checkpointer = utils.Checkpointer(variable_paths)
+
+    # Create a directory for an old checkpoint with this step, as if we are
+    # saving after an error/premption between the numpy checkpoint and the t5x
+    # checkpoint.
+    dir_already_there = os.path.join(checkpoint_dir,
+                                     'numpy_checkpoints',
+                                     f'checkpoint_{step}')
+    self.create_tempfile(dir_already_there)
+
+    with mock.patch.object(utils, 'np_save') as save_mock:
+      with mock.patch.object(utils.time, 'time') as time_mock:
+
+        def time_dispatch():
+          """Create fake return values for our time patch.
+
+          When we patch the time method, the patched version gets called by
+          other modules like logging. This means we can't use the simple
+          `.side_effect = (time, backup_time)` to return two values, one for
+          the initial call to make a tmp dir and one to use when creating the
+          backup. The number of these other calls seem to change as the number
+          of sets change so we can't just use a longer sequence.
+
+          Instead we check our call stack to see the context of the call.
+          We skip 0, 1, and 2 because those calls are for '_time_dispatch',
+          '_mock_call', and '__call__' which are mock calls and consistent.
+          The 3rd frame gives us the calling function and lets us detect if
+          we should return the backup time or not.
+
+          Returns:
+            The fake time.
+          """
+          fake_time = time
+          def _time_dispatch():
+            nonlocal fake_time
+            if inspect.stack()[3].function == 'save_numpy':
+              ret = fake_time
+              fake_time = backup_time
+              return ret
+            else:
+              return time
+          return _time_dispatch
+
+        time_mock.side_effect = time_dispatch()
+        checkpointer.save_numpy(parameters, step=step)
+
+    # Make sure the backup dir of the last checkpoint was created.
+    backup_dir = f'{dir_already_there}.backup-{backup_time}'
+    self.assertTrue(os.path.exists(backup_dir))
+
+    # Check that the final checkpoint is still correct.
     self.assertLen(save_mock.call_args_list, len(golds))
     call_args = sorted((call_args[0] for call_args in save_mock.call_args_list),
                        key=operator.itemgetter(0))
