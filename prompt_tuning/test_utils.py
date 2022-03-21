3c33d9b6c0560e47ba616f1df92e2d1ae2812bc6
Brian Lester <brianlester@google.com>
2022-03-21 11:38:03 -0700

    Update tests to with a Matcher Object to allow for using mock assert methods with Array arguments
    PiperOrigin-RevId: 436263093

---
+++ prompt_tuning/test_utils.py
@@ -0,0 +1,55 @@
+# Copyright 2022 Google.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Utilities for testing."""
+
+import jax.numpy as jnp
+
+
+class ArrayAllCloseMatcher:
+  """Allows arrays in mock asserts with jnp.allclose as the comparison."""
+
+  def __init__(
+      self, arr, rtol: float = 1e-5, atol=1e-08, equal_nan: bool = False):
+    super().__init__()
+    self.arr = arr
+    self.rtol = rtol
+    self.atol = atol
+    self.equal_nan = equal_nan
+
+  def __eq__(self, other):
+    return jnp.allclose(
+        self.arr,
+        other,
+        rtol=self.rtol,
+        atol=self.atol,
+        equal_nan=self.equal_nan)
+
+  def __repr__(self):
+    return repr(self.arr)
+
+
+class ArrayEqualMatcher:
+  """Allows arrays in mock asserts with jnp.array_equal as the comparison."""
+
+  def __init__(self, arr, equal_nan: bool = False):
+    self.arr = arr
+    self.equal_nan = equal_nan
+
+  def __eq__(self, other):
+    return jnp.array_equal(
+        self.arr, other, equal_nan=self.equal_nan)
+
+  def __repr__(self):
+    return repr(self.arr)
---
+++ prompt_tuning/test_utils_test.py
@@ -0,0 +1,113 @@
+# Copyright 2022 Google.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for our test utils."""
+
+from unittest import mock
+from absl.testing import absltest
+from absl.testing import parameterized
+import jax.numpy as jnp
+import numpy as np
+from prompt_tuning import test_utils
+
+DTYPES = (jnp.bfloat16, jnp.float32, jnp.int32)
+
+
+class ArrayMatcherTest(parameterized.TestCase):
+
+  @parameterized.product(
+      shape=[(2,), (4, 4), (1, 100)],
+      dtype1=DTYPES,
+      dtype2=DTYPES,
+      numpy1=[True, False],
+      numpy2=[True, False],
+      matcher=[test_utils.ArrayAllCloseMatcher, test_utils.ArrayEqualMatcher])
+  def test_equal(self, shape, dtype1, dtype2, numpy1, numpy2, matcher):
+    x = jnp.ones(shape, dtype=dtype1)
+    if numpy1:
+      x = np.array(x)
+    y = jnp.ones(shape, dtype=dtype2)
+    if numpy2:
+      y = np.array(y)
+    self.assertEqual(matcher(x), y)
+
+  @parameterized.product(
+      shape=[(2,), (4, 4), (1, 100)],
+      dtype1=DTYPES,
+      dtype2=DTYPES,
+      numpy1=[True, False],
+      numpy2=[True, False],
+      matcher=[test_utils.ArrayAllCloseMatcher, test_utils.ArrayEqualMatcher])
+  def test_equal_in_mock(self, shape, dtype1, dtype2, numpy1, numpy2, matcher):
+    x = jnp.ones(shape, dtype=dtype1)
+    if numpy1:
+      x = np.array(x)
+    y = jnp.ones(shape, dtype=dtype2)
+    if numpy2:
+      y = np.array(y)
+    self.assertEqual(matcher(x), y)
+    self.assertIsNot(x, y)
+
+    def run(fn):
+      return fn(y)
+
+    m = mock.MagicMock()
+    run(m)
+    m.assert_called_once_with(matcher(x))
+
+  @parameterized.product(
+      shape=[(2,), (4, 4), (1, 100)],
+      dtype1=DTYPES,
+      dtype2=DTYPES,
+      numpy1=[True, False],
+      numpy2=[True, False],
+      matcher=[test_utils.ArrayAllCloseMatcher, test_utils.ArrayEqualMatcher])
+  def test_not_equal(self, shape, dtype1, dtype2, numpy1, numpy2, matcher):
+    x = jnp.ones(shape, dtype=dtype1)
+    if numpy1:
+      x = np.array(x)
+    y = jnp.zeros(shape, dtype=dtype2)
+    if numpy2:
+      y = np.array(y)
+    self.assertNotEqual(matcher(x), y)
+
+  @parameterized.product(
+      shape=[(2,), (4, 4), (1, 100)],
+      dtype1=DTYPES,
+      dtype2=DTYPES,
+      numpy1=[True, False],
+      numpy2=[True, False],
+      matcher=[test_utils.ArrayAllCloseMatcher, test_utils.ArrayEqualMatcher],
+  )
+  def test_not_equal_in_mock(
+      self, shape, dtype1, dtype2, numpy1, numpy2, matcher):
+    x = jnp.ones(shape, dtype=dtype1)
+    if numpy1:
+      x = np.array(x)
+    y = jnp.zeros(shape, dtype=dtype2)
+    if numpy2:
+      y = np.array(y)
+    self.assertIsNot(x, y)
+
+    def run(fn):
+      return fn(y)
+
+    m = mock.MagicMock()
+    run(m)
+    with self.assertRaises(AssertionError):
+      m.assert_called_once_with(matcher(x))
+
+
+if __name__ == "__main__":
+  absltest.main()
