05da84dd6469401e2f7717f5fe5aa3077feee22d
Brian Lester <brianlester@google.com>
2022-05-13 13:45:17 -0700

    Update the `recreate_checkpoint` script to use Gin.
    Now that the PjitPartitioner infers sharding annotations/axes from metadata the model provideds when created, we can't just load a checkpoint off disk. We need to actual instantate the model itself. This is done via gin and implementation follows that of the t5x `convert_tf_checkpoint`.
    
    As part of this change, thing that used to be direct CLI arguments like `--step=N` are now set with gin overrides like `--gin.recreate_checkpoint.step=N`.
    
    PiperOrigin-RevId: 448569249

--- prompt_tuning/scripts/recreate_checkpoint.py
+++ prompt_tuning/scripts/recreate_checkpoint.py
@@ -28,50 +28,27 @@ surgery to splice the prompts numpy value for some given checkpoint into the
 final checkpoint. A common usecase being the recreation the best performing
 checkpoint to be used in things like the t5x `eval.py` script.
 
+Note:
+  This script only recreates the model *parameters* at some given step, the
+  optimizer state for the prompt variable at some step are not saved, thus the
+  optimizer state in the final checkpoint are not rewound like the parameters
+  are. This means this script can't be used to recreating an earlier
+  checkpoint and restart training from a previous step.
+
 """
 
 import os
-from typing import List, Sequence
-from unittest import mock
-from absl import app
-from absl import flags
+from typing import List, Optional
 from absl import logging
-import flax
-from flax import optim
 from flax import traverse_util
+import jax
 import jax.numpy as jnp
 import numpy as np
 from t5x import checkpoints
+from t5x import models
 from t5x import partitioning
-from t5x import state_utils
 from t5x import train_state as train_state_lib
 from tensorflow.io import gfile
-
-FLAGS = flags.FLAGS
-
-flags.DEFINE_string(
-    "model_dir",
-    None,
-    "The model directory where the checkpoints were written.")
-flags.DEFINE_integer(
-    "step", None, "The step number of the prompt to load.")
-flags.DEFINE_string("output_dir", None, "The output location.")
-flags.DEFINE_integer(
-    "concurrent_gb",
-    16,
-    "Maximum amount of memory to use at once, can use up to 4x in some cases.")
-flags.DEFINE_enum(
-    "save_dtype",
-    "float32",
-    ["float32", "bfloat16"],
-    "The data type to use while saving.")
-flags.DEFINE_enum(
-    "restore_dtype",
-    "float32",
-    ["float32", "bfloat16"],
-    "The datatype used while restoring.")
-flags.mark_flag_as_required("model_dir")
-flags.mark_flag_as_required("step")
 
 
 def find_numpy_checkpoints(model_dir: str, step: int) -> List[str]:
@@ -95,95 +72,125 @@ def file_name_to_variable_path(file_name: str) -> str:
   return variable_name.replace(".", "/")
 
 
-def build_optimizer(state_dict) -> optim.Optimizer:
-  """Create an (Adafactor) optimizer that matches `state_dict`."""
-  flat_state = state_utils.flatten_state_dict(
-      state_dict["state"]["param_states"])
-  none_keys = {f"/{k}" for k, v in flat_state.items() if v is None}
-
-  def filter_fn(path, _):
-    return path not in none_keys
-
-  optimizer_def = optim.MultiOptimizer(
-      (optim.ModelParamTraversal(filter_fn), optim.Adafactor()))
-  with mock.patch.object(flax.optim.base, "isinstance") as is_instance_mock:
-    is_instance_mock.side_effect = (
-        lambda x, y: True if y == jnp.ndarray else isinstance(x, y))
-    optimizer = optimizer_def.create(state_dict["target"])
-  return optimizer.restore_state(state_dict)
-
-
-def main(argv: Sequence[str]) -> None:
+def recreate_checkpoint(model: models.BaseModel,
+                        model_dir: str,
+                        step: int,
+                        concurrent_gb: int = 16,
+                        save_dtype: jnp.dtype = jnp.float32,
+                        output_dir: Optional[str] = None) -> None:
   """Add numpy checkpoint values at some step to a prompt tuning checkpoint."""
-  if len(argv) > 1:
-    raise app.UsageError("Too many command-line-arguments.")
-
-  output_dir = FLAGS.output_dir
+  # We are going to save the checkpoint with a new step number so if they don't
+  # give a new place to put it, just use the same dir.
   if output_dir is None:
-    output_dir = FLAGS.model_dir
+    output_dir = model_dir
+
+  save_dtype = jnp.dtype(save_dtype)
 
   # Find the latest checkpoint
-  latest_step = checkpoints.latest_step(FLAGS.model_dir)
+  latest_step = checkpoints.latest_step(model_dir)
   if latest_step is None:
-    raise ValueError(f"Cannot find checkpoint directory in {FLAGS.model_dir}")
+    raise ValueError(f"Cannot find checkpoint directory in {model_dir}")
   logging.info("Loading checkpoint at step %d", latest_step)
   checkpoint_directory = checkpoints.get_checkpoint_dir(
-      FLAGS.model_dir, latest_step)
+      model_dir, latest_step)
   logging.info("Loading checkpoint from %s", checkpoint_directory)
-  # Load the latest checkpoint
-  checkpoint = checkpoints.load_t5x_checkpoint(
-      checkpoint_directory,
-      restore_dtype=FLAGS.restore_dtype,
-      lazy_parameters=True)
 
-  flat_checkpoint = state_utils.flatten_state_dict(
-      checkpoint, keep_empty_nodes=True)
+  # Initialize the training state from the model which was loaded via gin.
+  def initialize_train_state(rng):
+    initial_variables = model.get_initial_variables(
+        rng=rng,
+        input_shapes={
+            "encoder_input_tokens": (1, 1),
+            "decoder_input_tokens": (1, 1)
+        })
+    return train_state_lib.FlaxOptimTrainState.create(model.optimizer_def,
+                                                      initial_variables)
+
+  train_state = jax.eval_shape(initialize_train_state, jax.random.PRNGKey(0))
+  # We are doing this conversion on a single host with capped memory usage
+  # so only 1 partitioned and keep params off devices.
+  partitioner = partitioning.PjitPartitioner(1, params_on_devices=False)
+  checkpointer = checkpoints.Checkpointer(
+      train_state, partitioner, output_dir, save_dtype=save_dtype)
+  train_state = checkpointer.restore(latest_step, lazy_parameters=True)
+
+  # Flatten the parameters so we can overwrite them.
+  flat_params = traverse_util.flatten_dict(train_state.params, sep="/")
 
   # Find the numpy files for the given step
   # TODO: Abstract this step to allow for more control of the
   # over-writing, i.e. given associated lists of variable overwrite paths and
   # paths to numpy files, overwrite several variables without being tied to
   # prompts produced by this training run.
-  numpy_files = find_numpy_checkpoints(FLAGS.model_dir, FLAGS.step)
+  numpy_files = find_numpy_checkpoints(model_dir, step)
   if not numpy_files:
-    raise ValueError(f"Cannot find any number checkpoints in {FLAGS.model_dir} "
-                     f"with step {FLAGS.step}")
+    raise ValueError(f"Cannot find any number checkpoints in {model_dir} "
+                     f"with step {step}")
+  # The numpy files for a checkpoint could include multiple parameters so
+  # loop through them.
   for numpy_file in numpy_files:
     logging.info("Loading numpy variable from %s", numpy_file)
     # Load the numpy variable
     with gfile.GFile(numpy_file, "rb") as f:
-      numpy_value = np.load(f).astype(FLAGS.restore_dtype)
+      numpy_value = np.load(f).astype(checkpointer.restore_dtype)
     # Figure out where the variable goes in the pytree
     variable_path = file_name_to_variable_path(numpy_file)
     logging.info("Overwriting the variable at %s with the value loaded from %s",
                  variable_path, numpy_file)
     # Update the variable in the pytree
-    flat_checkpoint[f"target/{variable_path}"] = numpy_value
-  # Set the step to the given step.
-  logging.info("Setting step to %d", FLAGS.step)
-  flat_checkpoint["state/step"] = np.asarray(
-      FLAGS.step, dtype=flat_checkpoint["state/step"].dtype)
+    flat_params[variable_path] = numpy_value
 
-  # Save the checkpoint with the given step prompt included.
-  checkpoint = traverse_util.unflatten_dict(
-      {tuple(k.split("/")): v for k, v in flat_checkpoint.items()})
-  partitioner = partitioning.PjitPartitioner(
-      num_partitions=1, params_on_devices=False)
-  # TODO: Add option to configure what optimizer to use.
-  optimizer = build_optimizer(checkpoint)
-  # Turn the optimizer into the train_state object
-  train_state = train_state_lib.FlaxOptimTrainState(optimizer)
-  train_state = train_state.restore_state(checkpoint)
-  checkpointer = checkpoints.Checkpointer(
-      train_state,
-      partitioner,
-      output_dir,
-      save_dtype=FLAGS.save_dtype,
-      restore_dtype=FLAGS.restore_dtype)
+  train_state = train_state.replace_params(
+      traverse_util.unflatten_dict(flat_params, sep="/"))
+  # Set the step to the given step.
+  logging.info("Setting step to %d", step)
+  # TODO: This conversion of the train step from an int to a numpy
+  # scalar should be upstreamed to the train_state as it currently allows one
+  # to save the step as a python integer which causes an error when reloading.
+  train_state = train_state.replace_step(np.array(step, dtype=save_dtype))
+
   logging.info("Saving result to %s", output_dir)
-  # Actually save the new checkpoint.
-  checkpointer.save(train_state, concurrent_gb=FLAGS.concurrent_gb)
+  # # Actually save the new checkpoint.
+  checkpointer.save(train_state, concurrent_gb=concurrent_gb)
 
 
 if __name__ == "__main__":
-  app.run(main)
+  # pylint:disable=g-import-not-at-top
+  from absl import flags
+  import gin
+  from t5x import gin_utils
+  # pylint:disable=g-import-not-at-to
+
+  FLAGS = flags.FLAGS
+
+  jax.config.parse_flags_with_absl()
+
+  flags.DEFINE_multi_string(
+      "gin_file",
+      default=None,
+      help="Path to gin configuration file. Multiple paths may be passed and "
+      "will be imported in the given order, with later configurations  "
+      "overriding earlier ones.")
+
+  flags.DEFINE_multi_string(
+      "gin_bindings", default=[], help="Individual gin bindings")
+
+  flags.DEFINE_list(
+      "gin_search_paths",
+      default=["third_party/py/t5x/configs",
+               "prompt_tuning/configs"],
+      help="Comma-separated list of gin config path prefixes to be prepended "
+      "to suffixes given via `--gin_file`. If a file appears in. Only the "
+      "first prefix that produces a valid path for each suffix will be "
+      "used.")
+
+  def main(_):
+    """True main function."""
+    recreate_checkpoint_using_gin = gin.configurable(recreate_checkpoint)
+
+    gin_utils.parse_gin_flags(FLAGS.gin_search_paths, FLAGS.gin_file,
+                              FLAGS.gin_bindings)
+    # Get gin-configured version of `recreate_checkpoint`.
+    recreate_checkpoint_using_gin()
+
+  gin_utils.run(main)
